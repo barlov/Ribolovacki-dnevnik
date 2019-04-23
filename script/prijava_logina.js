adf4d5cb3865d5b3e290fd1d2d5ba1634f5a06c4
barlov <barlov79@gmail.com>
2019-04-23 16:40:38 +0200

    Sign out radi

--- script/prijava_logina.js
+++ script/prijava_logina.js
@@ -1,11 +1,11 @@
 $.getScript('./script/skladiste_za_login.js'); 
-$.getScript('./script/skidanje_dropdown.js'); 
 
 
 function proveraLogina(){
    
     if(($.inArray($('#username').val(),imena1))!= -1 &&   ($.inArray($('#password').val(),lozinke1))!= -1 ){
         sessionStorage.setItem('ulogovaniKorisnik',$('#username').val());
-        skidanje();
+        location.reload();
+        
     } 
 }
